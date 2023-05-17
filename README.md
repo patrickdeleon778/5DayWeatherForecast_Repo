434e47e854312407dc75e134c3c6db5a143bb224
patrickdeleon778 <patrickdeleon778@gmail.com>
2023-05-17 00:14:30 -0400

    Commiting so I can turn in my sprint 3-4

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# 5DayWeatherForecast_Repo
+//================= Weather App ===================//
+
+Name: Patrick De Leon
+
+Date: 5/16/2023
+
+Reviewer:
+
+Peer Review:
+
+Github link: https://github.com/patrickdeleon778/5DayWeatherForecast_Repo.git
--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -4,8 +4,11 @@ let apikey = "&appid=a91a48b0aad8f376c03c5f0b1b9fb6f5";
 let units = "&units=imperial";
 let degSymbol = `&deg;F`;
 
+let temp = document.getElementById("temp");
 let temp_min = document.getElementById("temp_min");
 let temp_max = document.getElementById("temp_max");
+let climate = document.getElementById("climate");
+let feels_like = document.getElementById("feels_like");
 let place = document.getElementById("place");
 let favBtn = document.getElementById("favBtn");
 let delBtn = document.getElementById("delBtn");
@@ -19,40 +22,39 @@ let weatherArr = [];
 let searchedCity = "";
 
 let favData = JSON.parse(localStorage.getItem("favWeather"));
-// console.log(favData);
-if(favData && favData != null) {
+console.log(favData);
+// if(favData && favData != null) {
+//   favArr = favData;
+//   for (let i = 0; i < favData.length; i++) {
+//     if (i === 0) {
+//       fetchForecast(favData[i].url);
+//       let colDiv = document.createElement("div");
+//       colDiv.classList = "col";
 
-  favArr = favData;
-  for (let i = 0; i < favData.length; i++) {
-    if (i === 0) {
-      fetchForecast(favData[i].url);
-      let colDiv = document.createElement("div");
-      colDiv.classList = "col";
+//       let pTag = document.createElement("p");
+//       pTag.innerText = favData[i].name;
+//       pTag.addEventListener("click", (e) => {
+//         fetchForecast(favData[i].url);
+//       });
 
-      let pTag = document.createElement("p");
-      pTag.innerText = favData[i].name;
-      pTag.addEventListener("click", (e) => {
-        fetchForecast(favData[i].url);
-      });
-
-      colDiv.appendChild(pTag);
-      inject2.appendChild(colDiv);
+//       colDiv.appendChild(pTag);
+//       inject2.appendChild(colDiv);
       
-    } else {
-      let colDiv = document.createElement("div");
-      colDiv.classList = "col";
+//     } else {
+//       let colDiv = document.createElement("div");
+//       colDiv.classList = "col";
 
-      let pTag = document.createElement("p");
-      pTag.innerText = favData[i].name;
-      pTag.addEventListener("click", (e) => {
-        fetchForecast(favData[i].url);
-      });
+//       let pTag = document.createElement("p");
+//       pTag.innerText = favData[i].name;
+//       pTag.addEventListener("click", (e) => {
+//         fetchForecast(favData[i].url);
+//       });
 
-      colDiv.appendChild(pTag);
-      inject2.appendChild(colDiv);
-    }
-  }
-}
+//       colDiv.appendChild(pTag);
+//       inject2.appendChild(colDiv);
+//     }
+//   }
+// }
 
 btn.addEventListener("click", () => {
   fetchForecast(`${url_pt1}${search.value}${apikey}${units}`);
@@ -66,33 +68,45 @@ search.addEventListener("keypress", (e) => {
 
 favBtn.addEventListener("click", (e) => {
   let obj = {
-    name: place.innerText,
-    url: `${url_pt1}${searchedCity}${apikey}${units}`,
+    name: weatherArr[weatherArr.length - 1].city.name,
+    weatherData: weatherArr[weatherArr.length - 1]
+    // url: `${url_pt1}${searchedCity}${apikey}${units}`,
   };
-
+  addFavoriteCity(obj);
   favArr.push(obj);
-  let colDiv = document.createElement("div");
-  colDiv.classList = "col";
-  let pTag = document.createElement("p");
-  pTag.innerText = place.innerText;
-  pTag.addEventListener("click", (e) => {
-    fetchForecast(obj.url);
-  });
+  // let colDiv = document.createElement("div");
+  // colDiv.classList = "col";
+  // let pTag = document.createElement("p");
+  // pTag.innerText = place.innerText;
+  // pTag.addEventListener("click", (e) => {
+  //   fetchForecast(obj.url);
+  // });
 
-  colDiv.appendChild(pTag);
-  inject2.appendChild(colDiv);
+  // colDiv.appendChild(pTag);
+  // inject2.appendChild(colDiv);
 
   localStorage.setItem("favWeather", JSON.stringify(favArr));
 });
 
 delBtn.addEventListener("click", (e) => {
-  for (let i = 0; i < favArr.length; i++) {
-    if (place.innerText.toLowerCase() === favArr[i].name.toLowerCase()) {
-      favArr.splice(i, 1);
-      let colDiv = inject2.querySelector(".col");
-      inject2.removeChild(colDiv);
+  let currentCity = place.innerText;
+  let currentFav = inject2.getElementsByTagName('li');
+
+  for(let i = 0; i < currentFav.length; i++){
+    if(currentFav[i].innerText === currentCity){
+      inject2.removeChild(currentFav[i]);
+      break;
     }
   }
+
+
+  // for (let i = 0; i < favArr.length; i++) {
+  //   if (place.innerText.toLowerCase() === favArr[i].name.toLowerCase()) {
+  //     favArr.splice(i, 1);
+  //     let colDiv = inject2.querySelector(".col");
+  //     inject2.removeChild(colDiv);
+  //   }
+  // }
   localStorage.setItem("favWeather", JSON.stringify(favArr));
 });
 
@@ -109,10 +123,15 @@ function fetchForecast(url) {
 }
 
 function getForecast(forecastData) {
+    weatherArr = [];
+    weatherArr.push(forecastData);
+
     console.log(forecastData);
     place.innerText = forecastData.city.name; // changes the place id to the city name
+    temp.innerText = parseInt(forecastData.list[0].main.temp);
     temp_min.innerText = parseInt(forecastData.list[0].main.temp_min); // grabs the min temp and displays it
     temp_max.innerText = parseInt(forecastData.list[0].main.temp_max); // grabs the max temp and displays it
+    // climate.innerText = parseInt(forecastData.list[0].weather.description);
   
     inject.innerHTML = ""; // Makes it so whenever you search for a different city it'll wipe the old search and shows the new one
   
@@ -121,7 +140,7 @@ function getForecast(forecastData) {
     for (let i = 0; i < forecastData.list.length; i++) {
       let forecast = forecastData.list[i];
       let forecastDate = new Date(forecast.dt * 1000); // instead of every milisecond it'll be every second
-      let forecastDay = forecastDate.toLocaleDateString("en-US", { day: "numeric" }); // shows the day only. example: May 15th 2020 = 15
+      let forecastDay = forecastDate.toLocaleDateString("en-US", { day: "numeric" }); // shows the day only. example: May 15th 2023 = 15
     //   console.log(forecastDay);
   
       if (!dailyForecasts[forecastDay]) { // checks if there is no forecast stored for the current day 
@@ -139,6 +158,8 @@ function getForecast(forecastData) {
       forecastItem.classList.add("col");
       forecastItem.classList.add("forecastItem");
       forecastItem.classList.add("text-center");
+      forecastItem.classList.add("backgroundWhite");
+      forecastItem.classList.add("pixelFont");
   
       let date = document.createElement("p");
       date.classList.add("date");
@@ -170,8 +191,9 @@ function getForecast(forecastData) {
       description.classList.add("description");
       description.innerText = forecast.weather[0].description;
       forecastItem.appendChild(description);
-      forecastItem.style.paddingLeft = "15px";
-      forecastItem.style.paddingRight = "15px";
+      forecastItem.style.marginLeft = "15px";
+      forecastItem.style.marginRight = "15px";
+      forecastItem.style.paddingTop = "15px";
   
       inject.appendChild(forecastItem);
 
@@ -179,100 +201,46 @@ function getForecast(forecastData) {
     }
   }
   
+function addFavoriteCity(city){ // brought to you by zac. Thank you zac!
+  let newFav = document.createElement('li');
+  newFav.className = 'list-group-item';
+  newFav.innerText = city.name;
+  newFav.weatherData = city.weatherData;
+
+  inject2.appendChild(newFav);
+
+  // if(favData && favData != null) {
+  //   favArr = favData;
+  //   for (let i = 0; i < favData.length; i++) {
+  //     if (i === 0) {
+  //       fetchForecast(favData[i].url);
+  //       let newFav = document.createElement('li');
+  //       newFav.className = 'list-group-item';
+  
+  //       newFav.innerText = favData[i].name;
+  //       newFav.addEventListener('click', e => {
+  //         fetchForecast(favData[i].url);
+  //       });
+
+  //       inject2.appendChild(newFav);
+        
+  //     } else {
+  //       let newFav = document.createElement('li');
+  //       newFav.className = 'list-group-item';
+  
+  //       newFav.innerText = favData[i].name;
+  //       newFav.addEventListener('click', e => {
+  //         fetchForecast(favData[i].url);
+  //       });
+
+  //       inject2.appendChild(newFav);
+  //     }
+  //   }
+  // }
+
+  newFav.addEventListener('click', e => {
+    getForecast(newFav.weatherData);
+  })
+}
 
 fetchForecast(`${url_pt1}${city}${apikey}${units}`);
-
-
-
-
-
-
-// let url_pt1 = "https://api.openweathermap.org/data/2.5/weather?q=";
-// let city = "Roanoke";
-// let apikey = "&appid=a91a48b0aad8f376c03c5f0b1b9fb6f5";
-// let units = "&units=imperial";
-// let degSymbol = `&deg;F`;
-
-// //get all of our id's and and set them to a variable
-// let place = document.getElementById("place");
-// let temp = document.getElementById("temp");
-// let temp_min = document.getElementById("temp_min");
-// let temp_max = document.getElementById("temp_max");
-// let feels_like = document.getElementById("feels_like");
-// let speed = document.getElementById("speed");
-// let deg = document.getElementById("deg");
-// let search = document.getElementById("search");
-// let btn = document.getElementById("btn");
-// let favBtn = document.getElementById("favBtn");
-// let delBtn = document.getElementById("delBtn");
-// let injectFav = document.getElementById("inject");
-// let favArr = [];
-// let weatherArr = [];
-// let searchedCity = "";
-
-// let favData = JSON.parse(localStorage.getItem("favWeather"));
-// console.log(favData);
-// if(favData && favData != null) {
-
-//     favArr = favData;
-//   for (let i = 0; i < favData.length; i++) {
-//     if (i === 0) {
-//       fetchForecast(favData[i].url);
-//       let colDiv = document.createElement("div");
-//       colDiv.classList = "col";
-//       let pTag = document.createElement("p");
-//       pTag.innerText = favData[i].name;
-//       pTag.addEventListener("click", (e) => {
-//         fetchForecast(favData[i].url);
-//       });
-
-//       colDiv.appendChild(pTag);
-//       injectFav.appendChild(colDiv);
-//     } else {
-//       let colDiv = document.createElement("div");
-//       colDiv.classList = "col";
-//       let pTag = document.createElement("p");
-//       pTag.innerText = favData[i].name;
-//       pTag.addEventListener("click", (e) => {
-//         fetchForecast(favData[i].url);
-//       });
-
-//       colDiv.appendChild(pTag);
-//       injectFav.appendChild(colDiv);
-//     }
-//   }
-// }
-
-// //Delete button
-// delBtn.addEventListener("click", e => {
-//   for(let i = 0; i < favArr.length; i++){
-//     if(place.innerText.toLowerCase() === favArr[i].name.toLowerCase()){
-//       favArr.splice(i, 1);
-//       let colDiv = injectFav.getElementsByClassName('col');
-//       injectFav.removeChild(colDiv);
-//     }
-//   }
-//   localStorage.setItem('favWeather', JSON.stringify(favArr));
-// });
-
-
-// //fav button event listener
-// favBtn.addEventListener("click", (e) => {
-//   let obj = {
-//     name: weatherArr[weatherArr.length - 1].name,
-//     url: `${url_pt1}${searchedCity}${apikey}${units}`,
-//   };
-//   favArr.push(obj);
-//   let colDiv = document.createElement("div");
-//   colDiv.classList = "col";
-//   let pTag = document.createElement("p");
-//   pTag.innerText = search.value;
-//   pTag.addEventListener("click", (e) => {
-//     fetchForecast(obj.url);
-//   });
-
-//   colDiv.appendChild(pTag);
-//   injectFav.appendChild(colDiv);
-
-//   localStorage.setItem("favWeather", JSON.stringify(favArr));
-// });
